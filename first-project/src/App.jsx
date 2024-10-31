0b51131175ee07d2a02590ed9749fd13e95f918f
adel <mdbbsjkdiw129379@gmail.com>
2024-10-31 12:56:04 +0300

    first commit react

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,3 +1,17 @@
 export default function App(){
-return <p>jqbsssssss</p>
+    const Array = ['Item 1', 'Item 2', 'Item 3'] 
+    return (
+        <>
+          {Array.length === 0 ? (
+            <p>لا توجد عناصر في المصفوفة.</p>
+          ) : (
+            <ul>
+              {Array.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          )}
+        </>
+      );
+    
 }
